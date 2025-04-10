d2d22c530428baad612669192df75d12bae78c7c
MalayShikharSoni <malayshikhar2004@gmail.com>
2025-04-10 11:29:20 +0530

    Deserialisation vulnerability completed

--- Malay/Misconfig/vulnerable_dashboard.php
+++ Malay/Misconfig/vulnerable_dashboard.php
@@ -82,7 +82,7 @@ $user_sensitive_data = $sensitive_data[$user['id']];
         <div class="flex items-center justify-between space-x-10 max-md:space-x-4">
             <a href="/Kavy (Main Branch)/Home-Page.html#vulnerabilities" class="headerStuff  hover:cursor-[url('/Assets/Images/cursor_02.png'),_pointer] text-center font-semibold text-white hover:text-gray-300 max-md:text-xs">Vulnerabilities</a>
             <a href="/Kavy (Main Branch)/Home-Page.html" class="headerStuff  hover:cursor-[url('/Assets/Images/cursor_02.png'),_pointer] text-center font-semibold text-white hover:text-gray-300 max-md:text-xs max-md:hidden">Home</a>
-            <a href="#" class="headerStuff hover:cursor-[url('/Assets/Images/cursor_02.png'),_pointer]  text-center font-semibold text-white hover:text-gray-300 max-md:text-xs max-md:hidden">Contact Us</a>
+            <a href="/Karan/About Us/contact_us.html" class="headerStuff hover:cursor-[url('/Assets/Images/cursor_02.png'),_pointer]  text-center font-semibold text-white hover:text-gray-300 max-md:text-xs max-md:hidden">Contact Us</a>
         </div>
         
         <div id="menu-btn" class=" hover:cursor-[url('/Assets/Images/cursor_02.png'),_pointer] md:hidden focus:outline-none">
